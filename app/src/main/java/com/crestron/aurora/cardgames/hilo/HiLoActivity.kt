6a928d334c3947367bc12aa0efcc463955416e3f
jakepurple13 <jakerein@gmail.com>
2019-07-31 10:35:20 -0400

    -added past cards to HiLo

--- app/src/main/java/com/crestron/aurora/cardgames/hilo/HiLoActivity.kt
+++ app/src/main/java/com/crestron/aurora/cardgames/hilo/HiLoActivity.kt
@@ -1,14 +1,27 @@
 package com.crestron.aurora.cardgames.hilo
 
+import android.content.Context
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.OvershootInterpolator
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.crestron.aurora.R
 import com.crestron.aurora.utilities.AnimationUtility
 import com.crestron.aurora.utilities.ViewUtil
+import com.programmerbox.dragswipe.Direction
+import com.programmerbox.dragswipe.DragSwipeAdapter
+import com.programmerbox.dragswipe.DragSwipeUtils
 import crestron.com.deckofcards.Card
 import crestron.com.deckofcards.Deck
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 import kotlinx.android.synthetic.main.activity_hi_lo.*
+import kotlinx.android.synthetic.main.match_item.view.*
 
 class HiLoActivity : AppCompatActivity() {
 
@@ -29,6 +42,8 @@ class HiLoActivity : AppCompatActivity() {
 
     }
 
+    lateinit var adapter: HiLoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hi_lo)
@@ -40,6 +55,25 @@ class HiLoActivity : AppCompatActivity() {
         playerCard = deck!!.draw()
         player_card_view.setImageResource(playerCard!!.getImage(this))
         next.isEnabled = false
+
+        val list = arrayListOf<Card>()
+
+        adapter = HiLoAdapter(list, this)
+
+        hilo_past_cards.layoutManager = LinearLayoutManager(this).apply {
+            orientation = LinearLayoutManager.HORIZONTAL
+        }
+        hilo_past_cards.setHasFixedSize(true)
+        hilo_past_cards.adapter = adapter
+        hilo_past_cards.itemAnimator = SlideInUpAnimator(OvershootInterpolator())
+
+        DragSwipeUtils.setDragSwipeUp(adapter, hilo_past_cards, dragDirs = Direction.START + Direction.END)
+
+        hilo_show_past_cards.isChecked = true
+
+        hilo_show_past_cards.setOnCheckedChangeListener { _, b ->
+            hilo_past_cards.animate().alpha(if(b) 1f else 0f)
+        }
 
         fun clicking(condition: Boolean) = View.OnClickListener {
             if (mysteryCard == null) {
@@ -64,11 +98,11 @@ class HiLoActivity : AppCompatActivity() {
                             winned -> "You Win! W: ${++wins} L: $loses T:$ties"
                             else -> "You Lost! W: $wins L: ${++loses} T:$ties"
                         }
+
                         win_choose.append("\n${deck!!.deckCount()} Cards Left")
                         next.isEnabled = true
                         high.isEnabled = false
                         low.isEnabled = false
-
                     }
 
                 })
@@ -84,6 +118,7 @@ class HiLoActivity : AppCompatActivity() {
         low.setOnClickListener(clicking(false))
         next.setOnClickListener {
             if (mysteryCard != null) {
+                adapter.addItem(playerCard!!, 0)
                 chosen_cards.setImageResource(Card.BackCard.getImage(this@HiLoActivity))
                 playerCard = mysteryCard
                 player_card_view.setImageResource(playerCard!!.getImage(this@HiLoActivity))
@@ -99,4 +134,26 @@ class HiLoActivity : AppCompatActivity() {
         }
 
     }
+
+    class HiLoAdapter(list: ArrayList<Card>, val context: Context) : DragSwipeAdapter<Card, ViewHolder>(list) {
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val iv = ImageView(context)
+            iv.setImageResource(list[position].getImage(context))
+            holder.linearLayout.addView(iv)
+            //holder.cardView.setImageResource(list[position].getImage(context))
+            //holder.cardView.contentDescription = list[position].toString()
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.match_item, parent, false))
+        }
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //val cardView: ImageView = itemView.card_info_cards!!
+        val linearLayout: LinearLayout = itemView.match_card_item_layout!!
+    }
+
 }
